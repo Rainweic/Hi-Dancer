777a90df3ae6ad1c4403197a1b426b806b61a204
rainweic <861514706@qq.com>
2019-09-18 08:20:27 +0800

    ubuntu下无法显示画面 mac下视频尺寸不对 需要window下进行测试

--- DcDesigner/DcDesigner.py
+++ DcDesigner/DcDesigner.py
@@ -125,15 +125,21 @@ class SaveAction(QThread):
         '''
         保存当前帧拥有的动作骨架
         '''
+        self.playPause()
+
         if self.widget.videoCapTure == None:
             QMessageBox.warning(self.widget, "警告！", "请选择视频文件！")
             return None
 
         self.widget.videoCapTure.set(cv.CAP_PROP_POS_MSEC, int(self.widget.player.position()))    # 通过ms进行定位
         ret, frame = self.widget.videoCapTure.read()   # 获取帧
+        cv.imwrite("test.jpg", frame)
 
         # 骨架信息预测
         pred, img = net.detection(self.widget.model, frame, False)
+        if pred == None:
+            QMessageBox.warning(self.widget, "警告", "当前图像中未检测到人体，无法保存动作！")
+            return None
         if self.widget.isFirst:
             self.widget.dist, skeleton = tools.normalization(pred['pred_coords'][0])
             self.widget.isFirst = False
@@ -155,8 +161,13 @@ class SaveAction(QThread):
         with open(savePath, "w") as f:
             f.write(json.dumps(self.widget.poseInfo))
 
+    def playPause(self):
+        self.widget.playOrStop.setText("播放")
+        self.widget.play = False
+        self.widget.player.pause()
 
 if __name__ == "__main__":
+    os.environ["MXNET_CUDNN_AUTOTUNE_DEFAULT"] = str(0)
     app = QApplication(sys.argv)
     Dc = DcDesigner()
     Dc.show()
